89b00d44e3e69bef10d34c5b27f54f69cdd82114
Saimoon Saki <73993441+SaimoonSaki@users.noreply.github.com>
2026-05-02 22:46:39 +0600

    Update leaderboard-data.js

--- central/js/leaderboard-data.js
+++ central/js/leaderboard-data.js
@@ -16,13 +16,13 @@
 const CONTRIBUTORS = [
   {
     id: "founder",
-    name: "Founder",                    // ← replace with your name
+    name: "Example",                    // ← replace with your name
     role: "Founder & Curator",
     uploads: 12,
     subjects: ["Thermodynamics", "Fluid Mechanics", "Reaction Engineering"],
-    joined: "2024-01",
-    note: "Started it all. Still uploading.",
-    emoji: "👑",
+    joined: "2026-04",
+    note: "Isn't it cute?",
+    emoji: "https://instagram.fdac198-1.fna.fbcdn.net/v/t51.82787-19/683091109_17887221966500536_4580926789124968733_n.jpg?efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLmRqYW5nby4xMDc5LmMyIn0&_nc_ht=instagram.fdac198-1.fna.fbcdn.net&_nc_cat=104&_nc_oc=Q6cZ2gG6QHjdd30BuD5WmtQ0zRMb0liHbGGaB_Cxx8YyNfS9pZOXymj1hBDnOsxqfZoYJBY&_nc_ohc=XyMdoXzC9ukQ7kNvwFwxl0G&_nc_gid=i_9fa5phA90N7XCngTAQIA&edm=ALGbJPMBAAAA&ccb=7-5&oh=00_Af5CFv6nc0jR_ODY15Huv4P8SMhmRMnleI0vOU75zGoCmg&oe=69FBF548&_nc_sid=7d3ac5",
   },
   {
     id: "contributor-1",
