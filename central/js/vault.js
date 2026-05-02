b7e0db79c75f8b476a6f38eef0080867f37a4f34
Saimoon Saki <73993441+SaimoonSaki@users.noreply.github.com>
2026-05-02 22:33:07 +0600

    Update vault.js

--- central/js/vault.js
+++ central/js/vault.js
@@ -1,5 +1,5 @@
 // vault.js
-const STORAGE_KEY = "saimoonsaki";
+const STORAGE_KEY = "goat_vault_access";
 
 document.addEventListener("DOMContentLoaded", () => {
   if (localStorage.getItem(STORAGE_KEY) === "true") showVault();
